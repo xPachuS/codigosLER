756bfb1cb4c83b6b42a33a0eec4febcfed4e01da
xPachuS <albertouge@gmail.com>
2024-09-03 00:30:56 +0200

    Update scriptpass.js

--- scriptpass.js
+++ scriptpass.js
@@ -3,7 +3,7 @@ const correctPassword = "xPachuS";
 function checkPassword() {
     const enteredPassword = document.getElementById('password').value;
     if (enteredPassword === correctPassword) {
-        window.location.href = "pruebados.html"; // Cambia "pagina_protegida.html" a la URL de tu página protegida
+        window.location.href = "pruebatres.html"; // Cambia "pagina_protegida.html" a la URL de tu página protegida
     } else {
         document.getElementById('error').textContent = 'Contraseña incorrecta, por favor intente de nuevo.';
     }
